8b3cc95b6968d6ff0e518190c4b8cc57ba3f8048
Yuri Alves <yuripiresalves@gmail.com>
2024-08-12 17:42:46 -0300

    improve routes

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,29 +1,46 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Query
 from typing import List
 
-from features.slant import slant 
+from features.slant import slant
+from svm.main import train_and_test_svm
 
 app = FastAPI()
 
-@app.post("/manuscripts")
+train_data = [] 
+test_data = []   #
+
+@app.post("/manuscripts/training") 
 async def read_manus(images: List[UploadFile] = File(...)):
     """
-    Recebe uma lista de imagens de manuscritos e as processa.
-
-    Args:
-        images: Lista de arquivos de imagem do tipo UploadFile.
-
-    Returns:
-        Um dicionário com o resultado do processamento de cada imagem.
+    Recebe uma lista de imagens de manuscritos para treino.
     """
-    results = {}
+    
     for image in images:
-        # Lê o conteúdo do arquivo de imagem.
-        contents = await image.read() 
-        
-        # Chama a função slant para processar a imagem.
-        result = slant(contents) 
-        
-        # Adiciona o resultado ao dicionário de resultados.
-        results[image.filename] = result 
-    return results
+        contents = await image.read()
+        result = slant(contents)
+        train_data.append((result))
+
+    return {"message": f"Imagens de treino adicionadas com sucesso!"}
+
+@app.post("/manuscripts/test")
+async def test_manus(image: UploadFile = File(...)):
+    """
+    Recebe uma imagem de manuscrito para teste.
+    """
+    contents = await image.read()
+    result = slant(contents)
+    test_data.append(result)
+    return {"message": "Imagem de teste adicionada com sucesso!"}
+
+@app.get("/manuscripts/predict") 
+async def predict_manuscript(labels: int = Query(..., alias="labels")):
+    """
+    Treina o SVM e realiza a predição na imagem de teste.
+    """
+    if not train_data or not test_data:
+        return {"error": "Dados de treino ou teste não encontrados!"}
+
+
+    accuracy = train_and_test_svm(labels, train_data, test_data) 
+
+    return {"accuracy": accuracy} 
