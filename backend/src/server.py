19f10398a59beb7d91b29f816fecc625b9ff3d4c
Yuri Alves <yuripiresalves@gmail.com>
2024-08-28 11:30:51 -0300

    create histrogram

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI, File, HTTPException, UploadFile, Query
 from typing import List
+import matplotlib
+matplotlib.use('Agg')  # Define o backend para Agg
+import matplotlib.pyplot as plt
 
 from features.slant import slant
 from svm.main import train_and_test_svm
@@ -18,14 +21,32 @@ async def train_data_and_return(training_images : List[UploadFile] = File(...), 
     train_data = []
     test_data = []
     
-    for image in training_images:
+    for i, image in enumerate(training_images):
         contents = await image.read()
         result = slant(contents)
         train_data.append(result)
+        
+        # Criar e salvar gráfico do histograma de treino
+        plt.figure()
+        plt.bar(range(17), result)
+        plt.title(f"Histograma Imagem de Treino {i+1}")
+        plt.xlabel("Ângulo")
+        plt.ylabel("Frequência Normalizada")
+        plt.savefig(f"histograma_treino_{i+1}.png")
+        plt.close()  # Fechar a figura para liberar memória
 
     contents = await test_image.read()
     result = slant(contents)
     test_data.append(result)
+    
+     # Criar e salvar gráfico do histograma de teste
+    plt.figure()
+    plt.bar(range(17), result)
+    plt.title("Histograma Imagem de Teste")
+    plt.xlabel("Ângulo")
+    plt.ylabel("Frequência Normalizada")
+    plt.savefig("histograma_teste.png")
+    plt.close()  # Fechar a figura para liberar memória
 
     accuracy = train_and_test_svm(labels, train_data, test_data)
 
